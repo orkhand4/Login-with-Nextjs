fb32272bb64871dc8a8079c7660054935770e80f
Orkhand4 <orkhand4@mail.ru>
2025-03-16 13:12:36 +0400

    Nextjs Login JWT Practice

--- app/account/page.jsx
+++ app/account/page.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from "react";
+import { cookies } from "next/headers";
+import { verifyJWT } from "@/lib/auth";
 
-export default function AccountPage() {
-  return (
-    <div>
-      Account Page
-    </div>
-  )
+export default async function AccountPage() {
+  const cookieStore = cookies();
+  const token = cookieStore.get("token")?.value;
+
+  if (!token) {
+    return <p>You must be logged in to view this page.</p>;
+  }
+
+  try {
+    const { payload } = await verifyJWT(token);
+    return <h1>Welcome, {payload.username}</h1>;
+  } catch (error) {
+    return <p>Invalid or expired token. Please Log in again.</p>;
+  }
 }
